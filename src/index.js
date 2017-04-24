c2eebf8b36350086098bab7a4a063408804fa6ea
Daniel Winter <daniel@graphcms.com>
2017-04-24 08:30:53 +0200

    changed mutations: call each mutation one by one

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Lokka } from 'lokka';
 import { Transport } from 'lokka-transport-http';
 
-// ensure timezon is set to UTC:
+// ensure timezone is set to UTC:
 process.env.TZ = 'UTC'
 
 
@@ -10,36 +10,32 @@ const client = new Lokka({
   transport: new Transport('https://api.graphcms.com/simple/v1/$PROJECTID_OR_ALIAS$'),
 });
 
-const createEventMutation = ({ date, description, event, link, location, speakers, title }) => `
-  createEvent(
-    date: "${new Date(Date.parse(date)).toISOString()}"
-    description: "${description}"
-    event: "${event}"
-    isPublished: true
-    link: "${link}"
-    location: "${JSON.stringify(location).replace(/\"/g, '\\"')}"
-    speakers: ${JSON.stringify(speakers)}
-    title: "${title}"
-  ) {
-    id
-  }
-`;
-
+const createEvent = async ({ date, description, event, link, location, speakers, title }) => {
+  const mutationResult = await client.mutate(`{
+    event: createEvent(
+      date: "${new Date(Date.parse(date)).toISOString()}"
+      description: "${description}"
+      event: "${event}"
+      isPublished: true
+      link: "${link}"
+      location: "${JSON.stringify(location).replace(/\"/g, '\\"')}"
+      speakers: ${JSON.stringify(speakers)}
+      title: "${title}"
+    ) {
+      id
+    }
+  }`);
+  return mutationResult.event.id;
+};
 
 const startImport = async () => {
   const data = require('../data.json');
 
   console.log(`Start to import ${data.length} entries`);
 
-  // create a mutation for every event and assign it to an alias:
-  const mutations = data.map((event, index) => `mutation_${index}: ${createEventMutation(event)}`);
+  const eventIds = await Promise.all(data.map(createEvent));
 
-  const mutationString = `{
-    ${mutations.join(' ')}
-  }`;
-
-  const results = await client.mutate(mutationString);
-  console.log(`Finished creating ${Object.keys(results).length} events`, results);
+  console.log(`Finished creating ${eventIds.length} events. Ids:`, eventIds);
 };
 
 startImport().catch((err) => console.error('Error while importing data', err));
